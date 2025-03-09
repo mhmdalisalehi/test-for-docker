97560df6c87b3a555fb3ad8815ec594678b612ef
Mohammad Roudbari <mohammadroodbari2019@gmail.com>
2025-03-09 11:21:50 +0330

    Initial commit

--- core/website/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Photo(models.Model):
-    file = models.ImageField(upload_to="photos/")
-
-    # def __str__(self):
-    #     return self.file.name
